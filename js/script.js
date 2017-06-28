0510e78098b470e2c6ce63f9f8344141162b680d
HYNIX Jang <hynix_1@naver.com>
2017-06-28 14:17:19 +0900

    Background size

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
-
     /* Scroll Bar Effect */
     $("#menu").scrollupbar();
     $('a').smoothScroll();
+
+    /* Auto Height */
+    var height = window.innerHeight;
+    $("#intro, #job").height(height);
 });
